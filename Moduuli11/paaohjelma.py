59f827b3056efe138db6e234f96dd5dad59f8b97
Farnaz <farnaz.jamilpanah@metropolia.fi>
2024-11-20 22:56:28 +0200

    Create paaohjelma.py

---
+++ Moduuli11/paaohjelma.py
@@ -0,0 +1,9 @@
+from julkaisu import Julkaisu
+from julkaisu import Lehti
+from julkaisu import Kirja
+
+lehti1 = Lehti("Aku Ankka","Aki Hyyppä")
+kirja1 = Kirja("Hytti n:o 6 " ,"Rosa Likson", 200 )
+
+lehti1.tulosta_tiedot()
+kirja1.tulosta_tiedot()
